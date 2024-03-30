9fb9be54c32ee926d2ad49e6c1dff326d59eb975
AerousZero <srijanshakya77@gmail.com>
2024-03-30 22:20:13 +0545

    Job Models

--- backend/connect_kitchen_be/api/models.py
+++ backend/connect_kitchen_be/api/models.py
@@ -32,3 +32,16 @@ class Review(models.Model):
     chef = models.ForeignKey(FreelanceChef, on_delete=models.CASCADE)
     review_text = models.TextField()
     rating = models.FloatField()
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    fixed_budget = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    hourly_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    location = models.CharField(max_length=100)
+    deadline = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    required_skills = models.ManyToManyField(Skill, related_name='jobs', blank=True)
+    required_experience = models.IntegerField(default=0)
