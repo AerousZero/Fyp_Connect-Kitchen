c76f19ca53f4658f79dbd69d5d7c75e505b4e803
AerousZero <srijanshakya77@gmail.com>
2024-03-30 23:01:38 +0545

    UpdatedRegister.jsp

--- frontend/src/feature/Register/Register.jsx
+++ frontend/src/feature/Register/Register.jsx
@@ -1,151 +1,173 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { signUp } from "../../api/auth";
+import { notification } from "antd";
 
-const INITIAL_STATE = {
-  email: "",
-  password: "",
-  first_name: "",
-  last_name: "",
-  username: "",
-  role: "chef",
-};
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
 
-const Register = () => {
+const Register = (role) => {
+  const navigate = useNavigate();
+  const INITIAL_STATE = {
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: "",
+    username: "",
+    role: role,
+  };
   const [credentials, setCredentials] = useState(INITIAL_STATE);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setCredentials({ ...credentials, [name]: value });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    setLoading(true);
-
+  const registerHandler = async (data) => {
     try {
-      // Replace with actual registration logic
-      await register(credentials);
-      // Handle successful registration (redirect, message)
-      console.log("Registration successful!");
+      data.role = role;
+      const response = await signUp(data);
+      console.log(response, "response,");
+      if (response.status === 201) {
+        notification.success({ message: response.data.message });
+        navigate("/login");
+      } else {
+        notification.error({ message: "An unexpected error occurred" });
+      }
     } catch (error) {
-      // Handle registration errors
-      setError("Registration failed. Please try again.");
-    } finally {
-      setLoading(false);
+      if (error.response && error.response.status === 400) {
+        notification.error({
+          message: "Bad Request: " + error.response.data.message,
+        });
+      } else {
+        notification.error({ message: "An unexpected error occurred" });
+      }
     }
   };
 
   return (
     <div className="min-h-screen flex flex-col justify-center sm:py-12">
-      <div className="p-10 bg-white mx-auto w-[50%] rounded-lg shadow-lg border border-gray-200">
-        <h2 className="text-3xl font-semibold text-gray-800 mb-6">Register</h2>
-        {error && <div className="text-red-500 mb-4">{error}</div>}
-        <form onSubmit={handleSubmit}>
+      <div className="p-10 bg-white mx-auto max-w-lg rounded-lg shadow-lg border border-gray-200">
+        <h2 className="text-3xl font-semibold text-gray-800 mb-6">
+          Register as {capitalizeFirstLetter(role.role)}
+        </h2>
+        <form onSubmit={handleSubmit(registerHandler)}>
           <div className="mb-6 flex flex-col">
-            <label
-              htmlFor="first_name"
-              className="text-gray-700 text-sm font-bold mb-2"
-            >
-              First Name
+            <label htmlFor="email" className="mb-2 text-sm font-medium">
+              FirstName:
             </label>
             <input
-              type="text"
-              id="first_name"
-              name="first_name"
-              value={credentials.first_name}
-              onChange={handleChange}
-              className="w-full px-4 py-3 rounded-lg border focus:border-blue-500 focus:outline-none"
-              placeholder="Enter your first name"
-              required
+              type="firstName"
+              id="firstName"
+              {...register("firstName", { required: "firstName is required." })}
+              className={`w-full px-4 py-3 rounded-lg border-2 border-black focus:border-blue-500 focus:outline-none ${
+                errors.username ? "border-red-500" : ""
+              }`}
+              placeholder="Enter your firstName"
             />
+            {errors.username && (
+              <span className="text-red-500 text-sm">
+                {errors.firstName.message}
+              </span>
+            )}
           </div>
           <div className="mb-6 flex flex-col">
-            <label
-              htmlFor="last_name"
-              className="text-gray-700 text-sm font-bold mb-2"
-            >
-              Last Name
+            <label htmlFor="email" className="mb-2 text-sm font-medium">
+              LastName:
             </label>
             <input
-              type="text"
-              id="last_name"
-              name="last_name"
-              value={credentials.last_name}
-              onChange={handleChange}
-              className="w-full px-4 py-3 rounded-lg border focus:border-blue-500 focus:outline-none"
-              placeholder="Enter your last name"
-              required
+              type="LastName"
+              id="LastName"
+              {...register("lastName", { required: "LastName is required." })}
+              className={`w-full px-4 py-3 rounded-lg border-2 border-black focus:border-blue-500 focus:outline-none ${
+                errors.username ? "border-red-500" : ""
+              }`}
+              placeholder="Enter your LastName"
             />
+            {errors.lastName && (
+              <span className="text-red-500 text-sm">
+                {errors.lastName.message}
+              </span>
+            )}
           </div>
           <div className="mb-6 flex flex-col">
-            <label
-              htmlFor="email"
-              className="text-gray-700 text-sm font-bold mb-2"
-            >
-              Email Address
+            <label htmlFor="email" className="mb-2 text-sm font-medium">
+              Email:
             </label>
             <input
-              type="email"
-              id="email"
-              name="email"
-              value={credentials.email}
-              onChange={handleChange}
-              className="w-full px-4 py-3 rounded-lg border focus:border-blue-500 focus:outline-none"
-              placeholder="Enter your email"
-              required
+              type="Email"
+              id="Email"
+              {...register("email", { required: "Email is required." })}
+              className={`w-full px-4 py-3 rounded-lg border-2 border-black focus:border-blue-500 focus:outline-none ${
+                errors.username ? "border-red-500" : ""
+              }`}
+              placeholder="Enter your Email"
             />
+            {errors.username && (
+              <span className="text-red-500 text-sm">
+                {errors.email.message}
+              </span>
+            )}
           </div>
           <div className="mb-6 flex flex-col">
-            <label
-              htmlFor="username"
-              className="text-gray-700 text-sm font-bold mb-2"
-            >
-              Username
+            <label htmlFor="email" className="mb-2 text-sm font-medium">
+              Username:
             </label>
             <input
-              type="text"
+              type="username"
               id="username"
-              name="username"
-              value={credentials.username}
-              onChange={handleChange}
-              className="w-full px-4 py-3 rounded-lg border focus:border-blue-500 focus:outline-none"
-              placeholder="Choose a username"
-              required
+              {...register("username", { required: "Username is required." })}
+              className={`w-full px-4 py-3 rounded-lg border-2 border-black focus:border-blue-500 focus:outline-none ${
+                errors.username ? "border-red-500" : ""
+              }`}
+              placeholder="Enter your username"
             />
+            {errors.username && (
+              <span className="text-red-500 text-sm">
+                {errors.username.message}
+              </span>
+            )}
           </div>
           <div className="mb-6 flex flex-col">
-            <label
-              htmlFor="password"
-              className="text-gray-700 text-sm font-bold mb-2"
-            >
-              Password
+            <label htmlFor="password" className="mb-2 text-sm font-medium">
+              Password:
             </label>
             <input
               type="password"
               id="password"
-              name="password"
-              value={credentials.password}
-              onChange={handleChange}
-              className="w-full px-4 py-3 rounded-lg border focus:border-blue-500 focus:outline-none"
+              {...register("password", { required: "Password is required." })}
+              className={`w-full px-4 py-3 rounded-lg border-2 border-black focus:border-blue-500 focus:outline-none text-black ${
+                errors.password ? "border-red-500" : ""
+              }`}
               placeholder="Enter your password"
-              required
             />
+            {errors.password && (
+              <span className="text-red-500 text-sm">
+                {errors.password.message}
+              </span>
+            )}
           </div>
           <button
             type="submit"
-            disabled={loading}
-            className="bg-blue-500 text-white rounded-lg px-4 py-3 font-semibold uppercase tracking-wide hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            className="w-full bg-green-500 text-white py-3 rounded-lg font-semibold hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
           >
-            {loading ? "Loading..." : "Register"}
+            Sign Up
           </button>
         </form>
-        <div className="text-gray-600 mt-4">
-          Already have an account?{" "}
-          <Link to="/login" className="text-blue-500 hover:underline">
-            Login here
+        <div className="text-sm text-center mt-4">
+          Don't have an account?{" "}
+          <Link to="/register" className="text-blue-500 hover:underline">
+            Login
           </Link>
         </div>
       </div>
