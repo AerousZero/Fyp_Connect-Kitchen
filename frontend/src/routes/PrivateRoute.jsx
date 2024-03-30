ca2c0c635388498e1130857bb3b572f63caa193e
AerousZero <srijanshakya77@gmail.com>
2024-03-30 23:41:01 +0545

    Public and private routes

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -4,14 +4,17 @@ import NotFound from "../page/NotFound";
 import ChefHomePage from "../page/Chef/HomePage";
 import ClientHomepage from "../page/Client/Homepage";
 import CreateJob from "../feature/Client/Jobs/CreateJob";
+import ClientProfile from "../page/Client/Profile";
 
 export const PrivateRoutes = () => {
   return (
     <Routes>
       <Route path="/chef/home" element={<ChefHomePage />} />
       <Route path="/client/home" element={<ClientHomepage />} />
+      
        <Route path="/client/create-job" element={<CreateJob />} />
       {/* <Route path="/admin/home" element={<Admin />} /> */}
+      <Route path="/client/profile" element={<ClientProfile />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
--- frontend/src/routes/PublicRoute.jsx
+++ frontend/src/routes/PublicRoute.jsx
@@ -3,12 +3,16 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from '../page/Login'; // Assuming LoginPage is the correct import
 import RegisterPage from '../page/Register';
 import NotFound from '../page/NotFound';
+import JoinOptions from '../page/JoinOptions';
+import ClientProfile from '../page/Client/Profile';
 
 export const PublicRoutes = () => {
     return (
         <Routes>
-            <Route path='/' element={<LoginPage />} />
-            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/' element={<JoinOptions />} />
+            <Route path='/register/:role' element={<RegisterPage />} />
+          
             <Route path='*'  element={<NotFound />} />
             
         </Routes>
