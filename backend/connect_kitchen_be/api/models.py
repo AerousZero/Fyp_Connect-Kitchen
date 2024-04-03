351aacb934419a1384293eb3087fd1d3ee747f74
AerousZero <srijanshakya77@gmail.com>
2024-04-03 14:50:01 +0545

    Admin Jazzmin Dashboard

--- backend/connect_kitchen_be/api/models.py
+++ backend/connect_kitchen_be/api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from enum import Enum
 
+#User Role
 class Role(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
@@ -11,12 +13,15 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
 
+#User Skill
 class Skill(models.Model):
     skill_name = models.CharField(max_length=100, unique=True)
 
 class Specialty(models.Model):
     specialty_name = models.CharField(max_length=100, unique=True)
 
+
+#Profile
 class FreelanceChef(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=100, blank=True, null=True)
@@ -30,15 +35,29 @@ class Review(models.Model):
     review_text = models.TextField()
     rating = models.FloatField()
 
+
 class Job(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     fixed_budget = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     hourly_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     location = models.CharField(max_length=100)
-    deadline = models.DateField()
+    duration = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    required_skills = models.ManyToManyField(Skill, related_name='jobs', blank=True)
+    required_skills = models.ManyToManyField(Skill)
     required_experience = models.IntegerField(default=0)
+
+
+
+#If Advertisement is implemented
+class Hospitality(models.Model):
+    title = models.CharField( max_length=50)
+    image = models.ImageField( upload_to='image/hospitality', null = True)
+
+class Advert(models.Model):
+    institute = models.ForeignKey(Hospitality, on_delete=models.CASCADE, related_name = 'advert')
+    name = models.CharField( max_length=50)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='image/advert', null=True)
