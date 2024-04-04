2bab72eb89cc63c2a04502efcd9923fc838f265e
Srijan Shakya <srijanshakya77@gmail.com>
2024-04-05 02:02:47 +0545

    Job.js Fetching Favourite Jobs

--- frontend/src/api/job.js
+++ frontend/src/api/job.js
@@ -16,3 +16,49 @@ export const addJob = async (data, token) => {
     throw error; // Re-throw error for handling at calling location
   }
 };
+
+export const fetchJob = async (token) => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/job/getJob/",
+  
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      return response;
+    } catch (error) {
+      throw error; // Re-throw error for handling at calling location
+    }
+  };
+  
+  export const fetchSavedJob = async (token) => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/job/saved/",
+  
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      return response;
+    } catch (error) {
+      throw error; // Re-throw error for handling at calling location
+    }
+  };
+  
+  export const whiteListJob = async (job, token) => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/job/save/",
+        { job },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      return response;
+    } catch (error) {
+      throw error; // Re-throw error for handling at calling location
+    }
+  };
+  
