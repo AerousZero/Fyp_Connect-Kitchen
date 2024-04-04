2f6f4d3e6ac1359223f2c7b679f2bce0511e5309
Srijan Shakya <srijanshakya77@gmail.com>
2024-04-05 02:17:06 +0545

    Tab.jsx file update

--- frontend/src/components/common/Tab/TabContent.jsx
+++ frontend/src/components/common/Tab/TabContent.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import Card from '../Card/index';
+import React, { useState, useEffect } from "react";
+import Card from "../Card/index";
+import { fetchJob, fetchSavedJob } from "../../../api/job"; 
+import Cookies from "js-cookie";
 
 const TabContent = ({ label }) => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let response;
+        const token = Cookies.get("accessToken"); 
+        if (label === "Saved") {
+         
+          response = await fetchSavedJob(token);
+        } else {
+          response = await fetchJob(token);
+        }
+        setJobs(response.data.data);
+      } catch (error) {
+        console.error("Error fetching job data:", error);
+      }
+    };
+
+    fetchData();
+  }, [label]); 
+
   return (
-    <div className='flex flex-wrap gap-4'>
-      <Card label={label} />
-      <Card label={label} />
-      <Card label={label} />
-      <Card label={label} />
-      <Card label={label} />
-      <Card label={label} />
+    <div className="flex flex-wrap gap-4">
+      {jobs.map((job) => (
+        <Card key={job.id} label={label} jobList={job} />
+      ))}
     </div>
   );
-}
+};
 
 export default TabContent;
